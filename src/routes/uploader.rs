a38ce5113d81023d1fa4032a352fa5e29a70f1cc
Kaue Pereira <kauefontes@outlook.com>
2024-10-04 01:05:41 -0400

    feat: created module to upload files to aws s3 presigned urls

---
+++ src/routes/uploader.rs
@@ -0,0 +1,23 @@
+use reqwest::Client;
+use std::fs::File;
+use std::io::Read;
+use anyhow::Result;
+use axum::http::StatusCode;
+
+pub async fn upload_to_s3(output_path: &str, presigned_url: &str) -> Result<(), (StatusCode, String)> {
+    let mut file = File::open(output_path).map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    let client = Client::new();
+    let response = client.put(presigned_url)
+        .body(buffer)
+        .send()
+        .await.map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    if response.status().is_success() {
+        Ok(())
+    } else {
+        Err((StatusCode::INTERNAL_SERVER_ERROR, "Erro ao enviar arquivo para o S3".to_string()))
+    }
+}
