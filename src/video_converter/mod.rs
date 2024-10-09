57a5c4b3af9966e1a439b77fbd4ab0b09aedd724
Kaue Pereira <kauefontes@outlook.com>
2024-10-08 21:40:45 -0400

    feat: added video verification and logging

--- src/video_converter/mod.rs
+++ src/video_converter/mod.rs
@@ -1,6 +1,33 @@
+use std::process::Command;
+use serde_json::Value;
+use std::process::Stdio;
 use ffmpeg_cli::{FfmpegBuilder, File, Parameter};
 use futures::{future::ready, StreamExt};
-use std::process::Stdio;
+
+pub async fn has_aac_codec(input: &str) -> Result<bool, Box<dyn std::error::Error>> {
+    tracing::info!("Checking if the video has the aac codec");
+    tracing::info!("Input: {}", input);
+    let output = Command::new("ffprobe")
+        .arg("-v")
+        .arg("error")
+        .arg("-select_streams")
+        .arg("a:0")
+        .arg("-show_entries")
+        .arg("stream=codec_name")
+        .arg("-of")
+        .arg("json")
+        .arg(input)
+        .output()?;
+
+    let stdout = String::from_utf8(output.stdout)?;
+    let json: Value = serde_json::from_str(&stdout)?;
+
+    if let Some(codec_name) = json["streams"][0]["codec_name"].as_str() {
+        Ok(codec_name == "aac")
+    } else {
+        Ok(false)
+    }
+}
 
 pub async fn convert_to_apple_compatible(
     input: &str,
@@ -45,6 +72,7 @@ pub async fn convert_to_apple_compatible(
             output_result.status,
             std::str::from_utf8(&output_result.stderr).expect("Erro ao converter")
         );
+        tracing::debug!("Output: {}", output);
         Ok(output.to_string())
     } else {
         Err(Box::new(std::io::Error::new(
@@ -52,4 +80,13 @@ pub async fn convert_to_apple_compatible(
             "Erro na conversão",
         )))
     }
+}
+
+pub async fn process_video(input: &str, output: &str) -> Result<(), Box<dyn std::error::Error>> {
+    if has_aac_codec(input).await? {
+        println!("The video already has the aac codec.");
+    } else {
+        convert_to_apple_compatible(input, output).await?;
+    }
+    Ok(())
 }
